2124c224085621fdbc52e7a7a0a6587918568fa5
Sadge-Student <20092443@mail.wit.ie>
2022-09-15 14:26:33 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner : "Joe Bloggs",
+    previous_owners: [
+        { name: "Path Smith", address: "1 Main Street"},
+        { name: "Sheila Dwyer", address: "2 High Street"},
+    ],
     type : {
         make : "Toyota",
         model : "Corolla",
@@ -24,3 +28,4 @@ const aCar = {
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log(aCar.registration.year + '-' + aCar.registration.countyCode + '-' + aCar.registration.number)
   console.log(`It is a ${aCar.colour.exterior} car, ${aCar.mileage} mileage, with ${aCar.colour.interior.texture} interior...`)
+  console.log('First owner : ' + aCar.previous_owners[0].name )
