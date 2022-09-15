8a3c86e4ed30075c175d4558f5eb5f6e5e413933
Sadge-Student <20092443@mail.wit.ie>
2022-09-15 14:31:34 +0100

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -9,6 +9,9 @@ const aCar = {
         model : "Corolla",
         cc : "1.8"
     },
+    features : [
+        'Parking assist', 'Alarm', 'Tow-bar'
+    ],
     registration : {
         year : "201",
         countyCode : "WD",
@@ -29,3 +32,15 @@ const aCar = {
   console.log(aCar.registration.year + '-' + aCar.registration.countyCode + '-' + aCar.registration.number)
   console.log(`It is a ${aCar.colour.exterior} car, ${aCar.mileage} mileage, with ${aCar.colour.interior.texture} interior...`)
   console.log('First owner : ' + aCar.previous_owners[0].name )
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+} 
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name) ;
+} 
+
+for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
